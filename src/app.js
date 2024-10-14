3aeeb33f3816c02094d3b85ef1e96e93b9907fc1
RaulMF40 <raulmf.30@gmail.com>
2024-10-14 09:21:26 +0200

    flyiodeploy branch

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ app.use(express.json())
 
 // Ruta básica
 app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello World gral!' })
+  res.status(200).json({ message: 'Hello World desde Jenkins y Flyio!' })
 })
 
 // Ruta de ejemplo para pruebas
